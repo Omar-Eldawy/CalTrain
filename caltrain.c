5072469cbdf02dff8210d9def866bfa5f319963a
Omar-Eldawy <eng.omar.aldawy@gmail.com>
2024-04-11 07:52:36 +0200

    comments

--- caltrain.c
+++ caltrain.c
@@ -3,28 +3,25 @@
 #include "caltrain.h"
 
 void
-station_init(struct station *station)
-{
-	station->waiting_passengers = 0;
+station_init(struct station *station) {
+    station->waiting_passengers = 0;
     station->empty_seats = 0;
     station->passengers_on_board = 0;
     pthread_mutex_init(&station->lock, NULL);
     pthread_cond_init(&station->available_train, NULL);
-    pthread_cond_init(&station->train_can_go,NULL);
+    pthread_cond_init(&station->train_can_go, NULL);
 }
 
 void
-station_load_train(struct station *station, int count)
-{
-    //only one train can be loaded at a time no need for mutual exclusion but for consistency I used it
-    //check if the train has empty seats and there are passengers waiting otherwise train leaves the station
+station_load_train(struct station *station, int count) {
     pthread_mutex_lock(&station->lock);
 
-        if(count && station->waiting_passengers) {
+        //check if the train has empty seats and there are passengers waiting otherwise train leaves the station
+        if (count && station->waiting_passengers) {
             station->empty_seats = count;
 
-            //while loop to avoid spurious wakeups
-            while(station->empty_seats && station->waiting_passengers) {
+            //while loop to avoid spurious wakeup
+            while (station->empty_seats && station->waiting_passengers) {
                 //wake up all passengers waiting for the train
                 pthread_cond_broadcast(&station->available_train);
 
@@ -39,8 +36,7 @@ station_load_train(struct station *station, int count)
 }
 
 void
-station_wait_for_train(struct station *station)
-{
+station_wait_for_train(struct station *station) {
     pthread_mutex_lock(&station->lock);
 
         station->waiting_passengers++;
@@ -48,7 +44,7 @@ station_wait_for_train(struct station *station)
         //wait for a train to arrive
         //as there is no train available, no empty seats or passengers on board
         //but when the train arrives, empty seats will be available and passengers on board remains 0
-        while(station->passengers_on_board == station->empty_seats) {
+        while (station->passengers_on_board == station->empty_seats) {
             pthread_cond_wait(&station->available_train, &station->lock);
             //passengers try to board the train
             //check if there are empty seats remaining
@@ -62,8 +58,7 @@ station_wait_for_train(struct station *station)
 }
 
 void
-station_on_board(struct station *station)
-{
+station_on_board(struct station *station) {
     pthread_mutex_lock(&station->lock);
 
         //passenger has sat down
@@ -72,7 +67,7 @@ station_on_board(struct station *station)
 
         //if there are no more empty seats or no more passengers waiting for the train
         //and all passengers on board sat down, train can leave
-        if(!station->empty_seats || (!station->passengers_on_board && !station->waiting_passengers)) {
+        if (!station->empty_seats || (!station->passengers_on_board && !station->waiting_passengers)) {
             pthread_cond_signal(&station->train_can_go);
         }
 
